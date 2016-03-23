e1d82b941a0381c0489282bae0dbc77d2c87ec2d
andrixnet <andrixnet@yahoo.com>
2016-03-23 19:20:31 +0200

    Started working on new configurations.
    Step 1: created settingsGlue.inc.php
        (still unused)
    Step 2: created new configuration options in settingsDefault.php
        (none applied)
    
    All variables are fully documented in settingsDefault.php.
    
    TODO:
    * port from settings.inc.php the remaining variables
        (mainly maps and contact infos)
    * rewrite existing variables in settingsDefault.php
    * write the glue file
    * rewrite the code to use the new variables.
    * write a new settings-example.inc.php
    
    ADMIN TODO:
    * take new settings-example.inc.php and recreate settings.inc.php
        with new variables
    * when ready, remove glue file.

--- lib/settingsDefault.inc.php
+++ lib/settingsDefault.inc.php
@@ -7,6 +7,269 @@
 
 require_once __dir__ . '/cache.php';
 
+/* ************************************************************************
+ * Server configuration.
+ * REQUIRED!!!
+ * Each node must explicitely redefine the relevant configuration
+ * values in this section.
+ */
+
+// *** Database ***********************************************************
+
+// database server hostname. Defaults to localhost.
+// Unless MySQL/MariaDB is deployed on a different server,
+// this does not need to be changed.
+$config['server']['db']['host'] = 'localhost';
+// database server port. Defaults to 3306 (TCP/mysql)
+// Unless MySQL/MariaDB runs on a custom port,
+// this does not need to be changed.
+$config['server']['db']['port'] = '3306';
+
+// database name (schema).REQUIRED
+$config['server']['db']['schema'] = 'ocpl';
+// database username. REQUIRED
+$config['server']['db']['username'] = 'my_username';
+// database password. REQUIRED
+$config['server']['db']['password'] = 'my_password';
+
+// *** Paths **************************************************************
+
+// Document root for the PHP application. With trailing "/". REQUIRED.
+// (same as DocumentRoot directive in Apache httpd config)
+// Should be set explicitely as absolute path on hosting server.
+// Example: /path/to/opencaching-pl/
+$config['server']['path']['root'] = '/var/www//ocpl-root';
+
+// The following are outside the application root path and must be defined
+// as aliased directories in Apache httpd config.
+
+// Path to OC dynamic, outside the root path. REQUIRED
+$config['server']['path']['ocdynamic'] = '/var/www/ocpl-data/';
+// Path to OC admin, outside the root path. REQUIRED
+$config['server']['path']['ocadmin'] = '/var/www/ocpl-admin/';
+
+// *** Debugging **********************************************************
+
+// FIXME 
+// Do these variables get set somewhere, like taken from URL parameters?
+/* 
+//Debug?
+if (!isset($debug_page))
+    $debug_page = false;
+if (!isset($debug))
+    $debug = false;
+$develwarning = '';
+
+//site in service? Set to false when doing bigger work on the database to prevent error's
+if (!isset($site_in_service))
+    $site_in_service = true;
+*/
+
+// *** FIXME??? ***********************************************************
+/*
+$dbpconnect = false;
+
+$tmpdbname = 'test';
+
+// warnlevel for sql-execution
+$sql_errormail = 'rt@localhost';
+$sql_warntime = 1;
+
+// replacements for sql()
+$sql_replacements['db'] = $dbname;
+$sql_replacements['tmpdb'] = 'test';
+
+// safemode_zip-binary
+$config['server']
+$safemode_zip = '/var/www/ocpl/bin/phpzip.php';
+$zip_basedir = $dynbasepath . 'download/zip/';
+$zip_wwwdir = '/download/zip/';
+*/
+
+
+
+/* ************************************************************************
+ * Node configuration.
+ * REQUIRED!!!
+ * Each node must explicitely redefine the relevant configuration
+ * values in this section.
+ */
+
+// Node ID. REQUIRED.
+// Use 4 for local development.
+// See /docs/oc_nodes.txt for existing node IDs and how to obtain an ID.
+$config['node']['id'] = 4;
+// Website host name. REQUIRED
+$config['node']['hostname'] = 'localhost';
+// Website domain name. REQUIRED
+$config['node']['domainname'] = 'localdomain';
+// Webside URL. With trailing "/". REQUIRED.
+// Change it only if necessary. (ie. using https)
+$config['node']['url'] = 'http://' . $config['node']['hostname'] . '.' . $config['node']['domainname'] . '/';
+// Website name. REQUIRED.
+// Used to identify the creator of some content.
+$config['node']['name'] = 'Your website name';
+// Short name for the website (international use)
+$config['node']['shortname'] = 'OC XX';
+// Website pages title. REQUIRED
+$config['node']['title'] = 'Opencaching website';
+// Waypoint prefix. REQUIRED
+// Use 2 upper case letters only (from english 26 letter character set).
+// Preferrably starting with "O"
+$config['node']['waypoint_prefix'] = 'OX';
+
+// *** Cookies ************************************************************
+// Cookie settings (hostname dependent). Change only if necessary.
+$config['node']['cookie']['name'] = 'oc';
+$config['node']['cookie']['path'] = '/';
+$config['node']['cookie']['domain'] = '.' . $config['node']['domainname'];
+
+// *** Styling and personalization ****************************************
+// Style
+$config['node']['styles']['stylename'] = 'stdstyle';
+// Template
+$config['node']['styles']['template'] = $config['server']['path']['ocdynamic'] . 'tpl/' . $config['node']['styles']['stylename'] . '/html/';
+// Header
+$config['node']['styles']['header'] = '__standard_header_images_set'; // not implemented yet
+// Icons
+$config['node']['styles']['icons'] = '__standard_icons_set'; // not implemented yet
+// Flags
+$config['node']['styles']['flags'] = '__standard_flags_set'; // not implemented yet
+// Caches
+$config['node']['styles']['caches'] = '__standard_caches_set'; // not implemented yet
+// Attributes
+$config['node']['styles']['attributes'] = '__standard_attributes_set'; // not implemented yet
+
+// *** Administration *****************************************************
+
+// In your database, table `user`, set column `admin` to 1 for all users
+// that must have administrative privileges granted to OC Team (access to ADMIN MENU)
+
+// Super admin 
+// Super admin, in addition to OC Team privileges, can remove all logs and more
+// see admin_users.php. Super admin user must already be an admin.
+// set the value of `user_id` from table `user` here
+$config['node']['admin']['superadmin'] = '';
+
+// News settings
+// News requires approval?
+$config['node']['news']['approve'] = true;
+// Email to be notified about a news approval request
+$config['node']['news']['approve_email'] = 'rt@localhost';
+
+// *** Administration *****************************************************
+
+/* ************************************************************************
+ * Language and regional settings
+ * Change to your country specifics.
+ */
+ 
+// Default language for the website. (2 letter language code)
+$config['node']['lang']['language'] = 'pl'; 
+// Default country. (2 letter country codee)
+$config['node']['lang']['country'] = 'PL'; 
+// Default locale setting
+$config['node']['lang']['locale'] = 'pl-PL'; 
+// Date format (PHP definition)
+// FIXME ??? shouldn't this be taken from locale?
+$config['node']['lang']['date_format'] = 'Y-m-d';
+// Time format (PHP definition)
+// FIXME ??? shouldn't this be taken from locale?
+$config['node']['lang']['time_format'] = 'H:i';
+// Date and Time format (PHP definition)
+// FIXME ??? shouldn't this be taken from locale?
+$config['node']['lang']['datetime_format'] = 'Y-m-d H:i';
+
+// Default country list (for country selectors)
+$config['node']['lang']['countries'] = array('AT', 'BE', 'BY', 'BG', 'HR', 'CZ', 'DK', 'EE', 'FI', 'FR', 'GR', 'ES', 'NL', 'IE', 'LT', 'MD', 'DE', 'NO', 'PL', 'PT', 'SU', 'RO', 'SK', 'SI', 'CH', 'SE', 'TR', 'UA', 'IT', 'HU', 'GB',);
+// Default language list (for cache description selector)
+$config['node']['lang']['content_languages'] = array ('EN', 'PL', 'FR', 'DE', 'NL', 'RO',);
+
+
+ 
+/* ************************************************************************
+ * External API.
+ * Change with your own API keys.
+ */
+
+// *** Google API *********************************************************
+
+// Google map API key. 
+// How to get an API key: https://developers.google.com/maps/documentation/javascript/get-api-key
+$config['node']['api']['google']['key'] = ''; // <-- insert your API key here
+// Google map type
+// G_MAP_TYPE or _HYBRID_TYPE
+$config['node']['api']['google']['map_type'] = 'G_MAP_TYPE';
+// Set this to true to disable automatic translation of cache descriptions
+// Google translation service became a paid service.
+$config['node']['api']['google']['translation'] = true;
+
+// *** Garmin API *********************************************************
+
+// Garmin API key.
+// How to get an API key: http://www.garmindeveloper.com/web-device/garmin-communicator-plugin/get-your-site-key/
+// Garmin keys are tied to your website URL
+// Make sure you generate your API key using your website URL with trailing "/"
+$config['node']['api']['garmin']['key'] = ''; // <-- insert your API key here
+$config['node']['api']['garmin']['url'] = $config['node']['url'];
+
+// *** GeoKrety API *******************************************************
+
+// email of GeoKrety developer (used in GeoKretyApi.php for error notifications)
+$config['node']['api']['geokrety']['devel_email'] = 'stefaniak@gmail.com';
+
+
+
+/* ************************************************************************
+ * Opencaching game
+ * Change only if your node's rules differ from the defaults.
+ */
+
+// *** Opencaching game rules *********************************************
+
+// Hide coordinates from not logged-in users. Set to "true" to hide.
+$config['oc']['rules']['hide_coords'] = false;
+// Users are required to find XX caches before being allowed to publish one
+// Disable by setting to -1
+$config['oc']['rules']['required_finds'] = 10;
+// First XX caches published by user must be approved by OC Team
+// Disable by setting to 0
+// Force all caches to require approval by setting to 999999999
+$config['oc']['rules']['required_approval'] = 3;
+// Each user should have at most XX Own/personal caches
+$config['oc']['rules']['own_caches'] = 1;
+// Cache scoring (recommendation)
+// Minimum score
+$config['oc']['rules']['min_score'] = 0;
+// Maximum score
+$config['oc']['rules']['max_score'] = 4;
+
+// *** Rewards ************************************************************
+
+// Titled caches
+$config['oc']['titled_cache']['nr_found'] = 10;
+$config['oc']['titled_cache']['prefix'] = 'week';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $config = array(
     /**
      *Add button to a shop. Set true otherwise false
---
+++ lib/settingsGLUE.inc.php
@@ -0,0 +1,19 @@
+<?php
+    require __DIR__.'/settings.inc.php';
+
+/* ************************************************************************
+ * Settings glue file 
+ * Take existing settings and map them to new configuration 
+ * variables with defaults defined in settingsDefault.inc.php
+ */
+
+ 
+ 
+    
+    
+    
+    
+    
+    
+    
+?>
