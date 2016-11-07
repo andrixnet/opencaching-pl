86a62eb79b49f8bb28aa46b65b99594951b7a60c
andrixnet <andrixnet@yahoo.com>
2016-11-07 17:18:34 +0200

    Maintenance scripts updates
    - improved do-wget-url
    - added better /dev/null redirection in ocpl-update.extended.sh
    - updated docs
    - housekeeping

--- docs/system/do-wget-url.sh
+++ docs/system/do-wget-url.sh
@@ -2,13 +2,15 @@
 
 BASE_URL=http://YOUR_OPENCACHING_HOSTNAME/
 OUTPUT_DIR=/path/to/conjobs_output/
+WGET_CMD=/usr/bin/wget
 
-JOB=$1
-OUTPUT=$2
+if [ -z "$1" ]; then echo "Missing command argument!"; exit 1; fi
+if [ -z "$2" ]; then echo "Missing output argument!"; exit 1; fi
 
-if [ -z "$2"
-# Assume wget is in /usr/bin
-/usr/bin/wget -q ${BASE_URL}$1 -O ${OUTPUT_DIR}$2
+${WGET_CMD} -q ${BASE_URL}/$1 -O ${OUTPUT_DIR}/$2
 
-# b..dy s. wypisywane do pliku - jak si. je wypisze na stdout, to cron powiadomi
-cat $OUTPUT_DIR$2
+# Check if there was output (usually errors).
+# Output these to stdout so cron generates a notification email
+if [ -f "${OUTPUT_DIR}/$2" -a -s "${OUTPUT_DIR}/$2" ]; then
+    cat ${OUTPUT_DIR}/$2
+fi
